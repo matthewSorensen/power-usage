c4630d817a564ebdae2a83cb9c927b9df26f31b8
Matthew Sorensen <scholl.ma@gmail.com>
2012-04-27 09:22:43 -0700

    Totally mess with the coordinate system and all the scales/dimensions

--- stacked.js
+++ stacked.js
@@ -7,7 +7,10 @@ d3.json = function(_,call){
 	    "USA": {"wind": 400, "hydro": 23, "nuclear": 231, "coal": 21},
 	    "Morocco": {"wind": 40, "hydro": 23, "nuclear": 1, "coal": 2},
 	    "China": {"wind": 4100, "hydro": 213, "nuclear": 231, "coal": 12},
-	    "Peru": {"hydro":1000}
+	    "Peru": {"hydro":1000},
+	    "USSR": {"nuclear":1250,"coal":1250},
+	    "Canukistan": {"maple syrup":500},
+	    "Oregon": {"coal":250}
 	},
 	"sources":{
 	    "USA": "http://en.wikipedia.org/wiki/Electricity_generation",
@@ -15,48 +18,57 @@ d3.json = function(_,call){
 	    "China":"http://en.wikipedia.org/wiki/Electricity_generation"
 	}});};
 
+var foo = {};
+
 function buildChart(selector,file,dim){
     dim = defaultDim(dim);
     var svg = d3.select(selector).append("svg:svg")
 	.attr("width", dim.width)
 	.attr("height", dim.height)
-	.append("svg:g")
-	.attr("transform", translate(dim.margin, dim.height - dim.margin));
+	.append("svg:g");
 
     d3.json(file, function(dat) {
 
 	var data = simplifyData(dat),
 	sectors = transposeToLayout(data),
 	scale  = scales(dim, data, sectors),
-	
 	sector = svg.selectAll("g.sector")
 	    .data(sectors)
 	    .enter().append("svg:g")
 	    .attr("class", "sector")
+	    .style("stroke", function(_,i){return  d3.rgb(scale.sector(i)).darker();})
 	    .style("fill", function(d, i) { return scale.sector(i); }),
+
+	yscale = d3.scale.linear()
+	    .domain([0,4556])
+	    .range([0,430]),
 
 	rect = sector.selectAll("rect")
 	    .data(Object)
 	    .enter().append("svg:rect")
 	    .attr("x", function(d) { return scale.country(d.x); })
-	    .attr("y", function(d) { return -(scale.energy(d.y0)) - scale.energy(d.y); })
-	    .attr("height", function(d) { return scale.energy(d.y); })
+	    .attr("y",function(d){
+		var baseline = yscale(d.y0);
+		return dim.height - dim.margin - (yscale(d.y0) + yscale(d.y));
+	    })
+	    .attr("height", function(d){return yscale(d.y);})
 	    .attr("width", scale.country.rangeBand()),
 	
 	label = svg.selectAll("text")
 	    .data(scale.country.domain())
 	    .enter().append("svg:text")
 	    .attr("x", function(d) { return scale.country(d) + scale.country.rangeBand() / 2; })
-	    .attr("y", 6)
+	    .attr("y", dim.height- .75*dim.margin)
 	    .attr("text-anchor", "middle")
 	    .attr("dy", ".71em")
 	    .text(function(d){return d;}),
 
-	newScale = scale.energy.copy(),
-	newRange = newScale.range(),
+	newScale = d3.scale.linear()
+	    .domain([0,data.max])
+	    .range([dim.height-dim.margin,dim.margin+dim.button])
 
 	axis = d3.svg.axis()
-	    .scale(newScale.range([newRange[1],newRange[0]]))
+	    .scale(newScale)
 	    .ticks(5)
 	    .orient("right")
 	    .tickFormat(function(n){
@@ -64,7 +76,7 @@ function buildChart(selector,file,dim){
 	    });
 
 	svg.append("g")
-	    .attr("transform",translate(dim.width - 2*dim.margin - dim.right,-dim.height + 2*dim.margin))
+	    .attr("transform",translate(dim.width - dim.right - 1.75*dim.margin,0))
 	    .attr("class","axis")
 	    .call(axis);
 
@@ -74,22 +86,29 @@ function buildChart(selector,file,dim){
 	    .append("svg:rect")
 	    .attr("class","button")
 	    .attr("x",function(d,i){return dim.margin+105*i})
-	    .attr("y",2*dim.margin - dim.height)
-	    .attr("rx",5)
+	    .attr("y",0.5*dim.margin)
 	    .attr("width",100)
-	    .attr("height",30)
+	    .attr("height",dim.button)
+	    .style("stroke", function(_,i){return  d3.rgb(scale.sector(i)).darker();})
 	    .attr("fill",function(_,i){return scale.sector(i);});
 
     });
 }
 
 function defaultDim(dim){
-    return {
-	height: dim.height || 500,
-	width: dim.width || 960,
-	margin: dim.margin || 20,
-	right: 50
 
+    var height = dim.height || 500,
+    margin = dim.margin || 20,
+    button = 20,
+    chartHeight = height - 2*margin - button;
+
+    return {
+	height: height,
+	width: dim.width || 960,
+	margin: margin,
+	right: 50,
+	button: button,
+	chartHeight: chartHeight
     };}
 
 function simplifyData(data){
@@ -122,10 +141,10 @@ function scales(dim,data,layout){
 	    .rangeBands([0, dim.width - 2*dim.margin - dim.right],0.1)
 	    .domain(layout[0].map(function(d) { return d.x; })),
 	energy: d3.scale.linear()
-	    .range([0, dim.height - 2*dim.margin])
-	    .domain([0,d3.max(layout[layout.length - 1], function(d) { return d.y0 + d.y; })]),
+	    .range([0,dim.height-2*dim.marign-dim.button])
+	    .domain([0,data.max]),
 	// This is slightly poor - we need to match the colorbrewer's size to sector length.
-	sector: d3.scale.ordinal().domain(data.sectors).range(colorbrewer.GnBu[9])
+	sector: d3.scale.ordinal().domain(data.sectors).range(colorbrewer.Blues[9])
     };}
 
 function translate(x,y){
