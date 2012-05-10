d9d65c087804436df9d07fa2a7af9949111980ba
Matthew Sorensen <scholl.ma@gmail.com>
2012-05-09 21:29:17 -0700

    Switch over to the new data format

---
+++ energyData.js
@@ -0,0 +1,98 @@
+var data = {
+    "USA": {
+	"url":"foobar.html",
+	"Consumption":{
+	    "Oil": 40.536,
+	    "Natural Gas":24.856,
+	    "Coal": 25.159,
+	    "Hydro": 2.537,
+	    "Nuclear": 8.463,
+	    "Renewable":1.622
+	},
+	"Production":{
+	    "Oil":  20.425,
+	    "Natural Gas": 22.224, 
+	    "Coal": 26.946
+	}
+    },
+    "Dominican":{
+	"url":"foobar.html",
+	"Consumption":{
+	    "Oil": 0.251,
+	    "Natural Gas":0.021,
+	    "Coal": 0.022,
+	    "Hydro": 0.016
+	},
+	"Production":{}
+    },
+    "Peru":{
+	"url":"foobar.html",
+	"Consumption":{
+	    "Oil": 0.4,
+	    "Natural Gas":0.136,
+	    "Coal": 0.041,
+	    "Hydro": 0.19,
+	    "Renewable":0.005
+	},
+	"Production":{
+	    "Oil":  0.335,
+	    "Natural Gas": 0.136, 
+	    "Coal": 0.003
+	}
+    },
+    "China":{
+	"url":"foobar.html",
+	"Consumption":{	    
+	    "Oil": 19.543,
+	    "Natural Gas":3.881,
+	    "Coal": 89.609,
+	    "Hydro": 7.045,
+	    "Nuclear": 0.765,
+	    "Renewable":0.297
+	},
+	"Production":{
+	    "Oil":  9.046,
+	    "Natural Gas": 3.434, 
+	    "Coal": 87.87
+	}
+    },
+    "India":{
+	"url":"foobar.html",
+	"Consumption":{	    
+	    "Oil": 6.736,
+	    "Natural Gas":2.362,
+	    "Coal": 18.232,
+	    "Hydro": 1.088,
+	    "Nuclear": 0.214,
+	    "Renewable":0.184
+	},
+	"Production":{
+	    "Oil":  2.019,
+	    "Natural Gas": 1.92, 
+	    "Coal": 15.041
+	}
+    },
+    "Morocco":{
+	"url":"foobar.html",
+	"Consumption":{	    
+	    "Oil": 0.442,
+	    "Natural Gas":0.021,
+	    "Coal": 0.101,
+	    "Hydro": 0.008,
+	    "Renewable":0.003
+	},
+	"Production":{
+	    "Oil":  0.008,
+	    "Natural Gas": 0.002, 
+	}
+    },
+    "Senegal":{
+	"url":"foobar.html",
+	"Consumption":{	    
+	    "Oil": 0.086,
+	    "Natural Gas":0.001,
+	    "Coal": 0.005,
+	    "Hydro": 0.002
+	},
+	"Production":{"Natural Gas": 001}
+    }};
--- stacked.js
+++ stacked.js
@@ -3,8 +3,6 @@ function translate(x,y){
     return "translate(" + x + "," + y + ")";
 }
 function contrast(c){
-    // Better contrast...
-    
     var hsl = d3.hsl(c);
     return hsl.l < 0.5? hsl.brighter(2): hsl.darker(3);
 }
@@ -13,14 +11,12 @@ function centerInBand(scale){
 	return scale(i) + 0.5*scale.rangeBand(); 
     };
 }
-
 function labelsFit(s){
     var max = d3.max(s.domain().map(function(d){
 	return d.length * 10;
     }));
     return s.rangeBand() > max;
 }
-
 function labelMod(s){
     var max = d3.max(s.domain().map(function(d){
 	return d.length * 10;
@@ -30,123 +26,129 @@ function labelMod(s){
     return (max > band) ? Math.ceil(max/band) : 1;
 }
 
-var redrawDelay = 500;
 
-function multibar(){
-    var sizes = {button: 15, right: 50, margin: 20}, source = '';
-    function chart(target){
-	function onData(data){
-	    sizes = completeSizes(sizes,target);
-	    var layout = transpose(data),
-	        scales = new Scales(sizes,data,layout);
+function Context(img,axis,data,sizes,scales){
+    var duration = 0;
 
-	    var sector = target.selectAll("g.sector")
-		.data(layout)
-		.enter().append("svg:g")
-		.classed("sector",true)
-		.style("stroke", function(_,i){return  d3.rgb(scales.sector(i)).darker();})
-		.style("fill", function(_, i) { return scales.sector(i); }),	    
-	    rect = sector.selectAll("rect")
-		.data(Object)
-		.enter().append("svg:rect")
-		.classed("box",true)
-		.attr("x", function(d) { return scales.country(d.x); })
-		.attr("height",20)
-		.attr("width", scales.country.rangeBand())
-	    button = target.selectAll("g.button")
-		.data(data.sectors)
-		.enter()
-		.append("svg:g")
-		.classed("button",true)
-		.style("cursor","hand"),
-	    axis = d3.svg.axis()
-		.scale(d3.scale.linear().range([sizes.height - sizes.margin, sizes.margin+sizes.button]))
-		.ticks(5)
-		.orient("right"),
-	    mod = labelMod(scales.country);
-	   
-	    target.selectAll("text")
-		.data(scales.country.domain())
-		.enter()
-		.append("svg:text")
-		.filter(function(d,i){
-		    return !(i % mod);
-		})
-		.attr("x", centerInBand(scales.country))
-		.attr("y", sizes.height- .75*sizes.margin)
-		.attr("text-anchor", "middle")
-		.attr("dy", ".71em")
-		.text(id);
+    function redraw(type){
+	type = arguments.length ? type : 'all';
+	scale = scales.energy[type];
 
-	    target.append("g")
-		.attr("transform",translate(sizes.width - sizes.right - 1.75*sizes.margin,0))
-		.classed("axis",true)
-		.call(axis);
-	    
-	    button.append("svg:rect")
-		.attr("x",scales.button)
-		.attr("y",0.5*sizes.margin)
-		.attr("width",scales.button.rangeBand())
-		.attr("height",sizes.button)
-		.style("stroke", function(_,i){return  d3.rgb(scales.sector(i)).darker();})
-		.attr("fill",function(_,i){return scales.sector(i);});
-
-	    button.append("svg:text")
-		.attr("x", centerInBand(scales.button))
-		.attr("y", 0.5*sizes.margin)
-		.attr("text-anchor", "middle")
-		.attr("dy", 0.5*sizes.button+2.5)
-		.style("fill",function(i){return contrast(scales.sector(i));})
-		.text(id);
-
-	    function draw (type){	
-
-		scale = scales.energy[type];
-		target.selectAll(".button")
-		    .transition()
-		    .duration(500)
-		    .style("opacity",function(button){
-			if(button == "all") return 1;
-			return (button==type || type=="all")? 1:0.25;
-		    });
-		
-		target.selectAll(".box")
-		    .transition().duration(500)
-		    .attr("y",function(d){
-			switch(type){
-			case "all":    return  sizes.height - (sizes.margin + scale(d.y0) + scale(d.y));
-			case d.sector: return sizes.height - sizes.margin - scale(d.y);
-			default:       return sizes.height *2;		
-			}
-		    }).attr("height", function(d){
-			return (type == "all" || d.sector == type)? scale(d.y):0;
-		    });
-		
-
-		axis.tickFormat(function(n){
-		    return d3.format(" e")(n) + ' ' + data.units;
-		});
-
-		axis.scale(axis.scale().domain(scale.domain()));
-		target.selectAll(".axis").transition().call(axis);
-	    }
-	    draw('all');
-	    // This is an ugly hack, but there's some weird data-aliasing bug that's really evil. So just redraw...
-	    // setTimeout(function(){draw('all');},redrawDelay += 100);
-	    
-	    button.on("mousedown",draw);
-	}
-	if(typeof(source)=="string"){
-	    if(source == '') source = target.attr("data-source");
-	    d3.json(source,function(raw_data){
-		onData(completeData(source));
+	img.selectAll(".button").transition().duration(duration)
+	    .style("opacity",function(button){
+		if(button == "all") return 1;
+		return (button==type || type=="all")? 1:0.25;
 	    });
-	}else
-	    onData(completeData(source));
+	img.selectAll(".box").transition().duration(duration)
+	    .attr("height", function(d){
+		return (type == "all" || d.sector == type)? scale(d.y):0;
+	    })
+	    .attr("y",function(d){
+		switch(type){
+		case "all":    return  sizes.height - (sizes.margin + scale(d.y0) + scale(d.y));
+		case d.sector: return sizes.height - sizes.margin - scale(d.y);
+		default:       return sizes.height *2;		
+		}
+	    });
+	axis.tickFormat(function(n){
+	    return d3.format(" e")(n) + ' ' + "Quads";
+	});
+	axis.scale(axis.scale().domain(scale.domain()));
+	img.selectAll(".axis").transition().call(axis);
+
+	duration = 500;
+	return redraw;
+    }
+    
+    redraw.img = d3.functor(img);
+    redraw.data = d3.functor(data);    
+    
+    return redraw;
+}
+
+var redrawDelay = 0;
+ 
+function multibar(){
+    var sizes = {button: 15, right: 50, margin: 20}, data = {}, callback = null;
+    function chart(target){
+	data = completeData(data);
+
+	sizes = completeSizes(sizes,target);
+	var layout = transpose(data),
+	scales = new Scales(sizes,data,layout);	
+	var sector = target.selectAll("g.sector")
+	    .data(layout)
+	    .enter().append("svg:g")
+	    .classed("sector",true)
+	    .style("stroke", function(_,i){return  d3.rgb(scales.sector(i)).darker();})
+	    .style("fill", function(_, i) { return scales.sector(i); }),	    
+	rect = sector.selectAll("rect")
+	    .data(Object)
+	    .enter().append("svg:rect")
+	    .classed("box",true)
+	    .attr("x", function(d) { return scales.country(d.x); })
+	    .attr("height",20)
+	    .attr("width", scales.country.rangeBand())
+	button = target.selectAll("g.button")
+	    .data(data.sectors)
+	    .enter()
+	    .append("svg:g")
+	    .classed("button",true)
+	    .style("cursor","hand"),
+	axis = d3.svg.axis()
+	    .scale(d3.scale.linear().range([sizes.height - sizes.margin, sizes.margin+sizes.button]))
+	    .ticks(5)
+	    .orient("right"),
+	mod = labelMod(scales.country);
+	
+	target.selectAll("text")
+	    .data(scales.country.domain())
+	    .enter()
+	    .append("svg:text")
+	    .filter(function(d,i){
+		return !(i % mod);
+		})
+	    .attr("x", centerInBand(scales.country))
+	    .attr("y", sizes.height- .75*sizes.margin)
+	    .attr("text-anchor", "middle")
+	    .attr("dy", ".71em")
+	    .text(id);
+	
+	target.append("g")
+	    .attr("transform",translate(sizes.width - sizes.right - 1.75*sizes.margin,0))
+	    .classed("axis",true)
+	    .call(axis);
+	
+	button.append("svg:rect")
+	    .attr("x",scales.button)
+	    .attr("y",0.5*sizes.margin)
+	    .attr("width",scales.button.rangeBand())
+	    .attr("height",sizes.button)
+	    .style("stroke", function(_,i){return  d3.rgb(scales.sector(i)).darker();})
+	    .attr("fill",function(_,i){return scales.sector(i);});
+	
+	button.append("svg:text")
+	    .attr("x", centerInBand(scales.button))
+	    .attr("y", 0.5*sizes.margin)
+	    .attr("text-anchor", "middle")
+	    .attr("dy", 0.5*sizes.button+2.5)
+	    .style("fill",function(i){return contrast(scales.sector(i));})
+	    .text(id);
+	
+	var con = new Context(target,axis,data,sizes,scales);
+	// This is an ugly hack, but there's some weird data-aliasing bug that's really evil. So just redraw...
+	setTimeout(con,redrawDelay += 100);
+	button.on("mousedown",con);
+	
     }
     // Expose a modicum of configurability.
     chart.size   = function(key,val){ sizes[key] = val; return chart; };
-    chart.source = function(dat){source = dat; return chart;};
+    chart.data   = function(val){ data = val; return chart; };
+    chart.callback = function(f){
+	callback = f;
+	return chart;
+    };
+
     return chart;
 }
 /* Calculate various dimensions and data indexes/totals that we'll need */
@@ -158,34 +160,38 @@ function completeSizes(sizes,target){
     return sizes;
 }
 function completeData(data){
-    var perCountry = data.data, sectors = {}, max;
-    data.countries = d3.keys(perCountry);
+    var newData = {}, sectors = {}, max;
+
+    newData.countries = d3.keys(data);
     // First iterate throught every country and compute their total power use
     // At the same time, this finds all sectors and puts them in sectors
     max = new Object();
-    max.all =  d3.max(d3.values(perCountry).map(function(country){
-	d3.keys(country).map(function(sect){
+    max.all =  d3.max(d3.values(data).map(function(country){
+	d3.keys(country.Consumption).map(function(sect){
 	    sectors[sect] = 0;
 	});	
-	return d3.sum(d3.values(country));
+	return d3.sum(d3.values(country.Consumption));
     }));
 
-    data.sectors = d3.keys(sectors);
+    newData.sectors = d3.keys(sectors);
     // Then go throught all the sectors and find the max for each sector:
-    data.sectors.map(function(sec){
-	max[sec] = d3.max(d3.values(perCountry).map(function(d){return d[sec]}));
+    newData.sectors.map(function(sec){
+	max[sec] = d3.max(d3.values(data).map(function(d){
+	    return d.Consumption[sec] || 0;
+	}));
     });
     
-    data.sectors = ["all"].concat(data.sectors);
-    
-    data.max = max;
-    return data;
+    newData.sectors = ["all"].concat(newData.sectors);  
+    newData.max = max;
+    newData.raw = data;
+    return newData;
 }
 /* Juggle a bunch of indexes, and transpose the data into a bunch of layers */
 function transpose(data){
     return d3.layout.stack()(data.sectors.map(function(type){
 	return data.countries.map(function(country){
-	    return {x: country, y: data.data[country][type] || 0, sector:type};
+	    var countryData = data.raw[country].Consumption;
+	    return {x: country, y: countryData[type] || 0, sector:type};
 	});}));}
 
 /* Generate all of the scales we'll need - for x (country), y(power - for each sector and total), and color */
@@ -204,3 +210,8 @@ function Scales(sizes,data,layout){
 	.rangeBands([sizes.margin,sizes.width-sizes.margin],0.05);
 }
 
+d3.select("body")
+    .append("svg:svg")
+    .attr("width",960)
+    .attr("height",500)
+    .call(multibar().data(data));
